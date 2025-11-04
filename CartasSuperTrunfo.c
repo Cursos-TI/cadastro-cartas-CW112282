7a40e6164084c39469722ea693b03dea3a94ef91
CW112282 <estevescarloswitor@gmail.com>
2025-11-04 16:21:21 -0300

    Atualizar o CartasSuperTrunfo.c
    Mudança

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -15,7 +15,7 @@ printf (Carta 1);
     char ("cidade [fortaleza]";
     int ("população 567900");
     float ("área em Km2 987898000");
-    float (pib 76576789000);
+    float ("pib 76576789000");
     int (" pontos turísticos [50]");
     
 printf (Carta 2);
@@ -23,9 +23,9 @@ printf (Carta 2);
     char("código 12]")
     char("cidade [distrito federal]")
     int ("população 76940");
-    float(área em km2 435675000)
-    float("pib 7676800)";
-    int("pontos turísticos [20]")
+    float("área em km2 435675000");
+    float("pib 7676800");
+    int("pontos turísticos [20]");
     
     
 return 0;
