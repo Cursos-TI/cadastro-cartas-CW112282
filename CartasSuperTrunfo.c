47beb6d7d83bdafbe2ce1c1bce01042eaf9f7e2a
CW112282 <estevescarloswitor@gmail.com>
2025-11-04 16:05:46 -0300

    Atualizar o CartasSuperTrunfo.c
    Atualização

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,26 +6,26 @@
 
 int main() {
 
-Scanf("informações de cartas");
+scanf("informações de cartas");
   
-Printf (Carta 1);
+printf (Carta 1);
 
-    char (Estado ; Ceará);
-    char (Código da carta [10]) ;
-    char (nome da cidade [fortaleza];
-    int (população 567.900);
-    float (Área em Km2 987.898.000);
-    float (PIB = 765.767.890,00R$);
-    int (número de pontos turísticos [50]);
+    char ("estado ; ceará"");
+    char ("código da carta [10]") ;
+    char ("nome da cidade [fortaleza]";
+    int ("população 567900");
+    float ("área em Km2 987898000");
+    float ("pib = 76576789000");
+    int ("número de pontos turísticos [50]");
     
-Printf (Carta 2);
-    char (Estado;Brasília);
-    char(Código da carta [12]);
-    char(nome da cidade [Distrito Federal];
-    int (população 76.940);
-    float(área em km2 435.675.000);
-    float(PIB = 76.768,00 R$);
-    int(número de pontos turísticos [20]);
+printf (Carta 2);
+    char ("estado;brasília");
+    char("código da carta [12]")
+    char("nome da cidade [distrito federal]")
+    int ("população 76940");
+    float("área em km2 435675000)"
+    float("pib = 7676800)";
+    int("número de pontos turísticos [20]")
     
     
     
