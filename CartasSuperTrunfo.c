211deab056964bb248b73d43eb7843ac74245471
CW112282 <estevescarloswitor@gmail.com>
2025-11-04 15:49:07 -0300

    Atualizar o CartasSuperTrunfo.c
    Informações das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,31 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
 
-  // Área para entrada de dados
+Scanf("informações de cartas");
+  
+Printf (Carta 1);
 
-  // Área para exibição dos dados da cidade
+    char (Estado ; Ceará);
+    char (Código da carta [10]) ;
+    char (nome da cidade [fortaleza];
+    int (população 567.900);
+    float (Área em Km2 987.898.000);
+    float (PIB = 765.767.890,00R$);
+    int (número de pontos turísticos [50]);
+    
+Printf (Carta 2);
+    char (Estado;Brasília);
+    char(Código da carta [12]);
+    char(nome da cidade [Distrito Federal];
+    int (população 76.940);
+    float(área em km2 435.675.000);
+    float(PIB = 76.768,00 R$);
+    int(número de pontos turísticos [20]);
+    
+    
+    
+    
 
 return 0;
 } 
